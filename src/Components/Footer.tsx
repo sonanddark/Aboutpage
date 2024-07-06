584a9d4696a636e22ab54cbe1992968583b02670
Muhammad Ahmedm <muhammed_hm@hotmail.com>
2024-07-07 04:02:42 +0500

    Refactor footer and main page. Also add animation when text comes in viewport

--- src/Components/Footer.tsx
+++ src/Components/Footer.tsx
@@ -1,84 +1,62 @@
 import React from 'react';
-import { motion } from 'framer-motion';
-import PrimaryButton from './Button';
-import Image from 'next/image';
-import footerLogo from "../../public/GIFS/logoWhite.gif";
-import footerStampArrow from "../assets/footerStampArrow.svg"
-import AnimateText from './AnimateText'; // Ensure this is the correct path
 import TextAroundCircle from './TextAroundCircle';
-const textRadius = 100; // Update this with the appropriate value
-const repeatedText = 'Your repeated text here '; // Update this with the appropriate value
+import AnimateWhenInViewport from './AnimateWhenInViewport';
+import AnimatedButton from './AnimatedButton';
 
 const Footer = () => {
   return (
     <footer className="w-100">
       <div className="flex md:flex-row flex-col md:justify-between md:m-auto md:items-center md:h-[408.52px] bg-[#DF1780]">
         <div className="md:m-[68px]">
-          {/* <AnimateText
-            topText="LOUDER"
-            bottomText="THE BETTER"
-            topTextCustomClass="flex md:justify-start font-bold text-[77px] md:text-[87.51px] text-white ml-20"
-            bottomTextCustomClass="flex md:justify-center font-semibold md:text-[58px] text-[50px] text-white ml-20"
-          /> */}
+          <AnimateWhenInViewport 
+            textToBeAnimated='LOUDER'
+            cssClass='flex md:justify-start font-bold text-[77px] md:text-[87.51px] text-white ml-20'/>
+          <AnimateWhenInViewport 
+            textToBeAnimated='THE BETTER'
+            cssClass='flex md:justify-center font-semibold md:text-[58px] text-[50px] text-white ml-20'/>
           <div className="md:block hidden">
-            {/* <PrimaryButton
-              customClassName={{
-                marginTop: "20px",
-                backgroundColor: "white",
-                border: "none",
-                color: "#DF1780",
-                padding: "10px 20px",
-                cursor: "pointer",
-                overflow: "hidden",
-                position: "relative",
-                display: 'flex',
-                alignItems: 'center',
-                fontSize: '15.25px',
-                lineHeight: "25.6px",
-                fontWeight: '590',
-                fontFamily: "SFProDisplay, sans-serif"
-              }}
+          <AnimatedButton
+              customCssStyles={{
+              marginLeft: "25px",
+              marginTop: "20px",
+              backgroundColor: "white",
+              border: "none",
+              color: "#DF1780",
+              padding: "10px 20px",
+              cursor: "pointer",
+              overflow: "hidden",
+              position: "relative",
+              display: 'flex',
+              alignItems: 'center',
+              fontSize: '15.25px',
+              lineHeight: "25.6px",
+              fontWeight: '590',
+              fontFamily: "SFProDisplay, sans-serif"
+            }}
               text="LET'S GO"
-            /> */}
+              arrowStyle={{
+                marginLeft: '8px',
+                transition: 'transform 0.3s ease',
+                stroke: '#DF1780'
+              }}
+            />
           </div>
         </div>
         <div className="flex md:justify-end items-center md:mr-10 mr-[0.5]">
-          <div className="md:hidden block">
-            <PrimaryButton
-              customClassName={{
-                marginLeft: "25px",
-                marginTop: "20px",
-                backgroundColor: "white",
-                border: "none",
-                color: "#DF1780",
-                padding: "10px 20px",
-                cursor: "pointer",
-                overflow: "hidden",
-                position: "relative",
-                display: 'flex',
-                alignItems: 'center',
-                fontSize: '15.25px',
-                lineHeight: "25.6px",
-                fontWeight: '590',
-                fontFamily: "SFProDisplay, sans-serif"
-              }}
-              text="LET'S GO"
-            />
-          </div>
-          <div className="flex justify-end gap-x-7">
-            <Image alt="footerLogo" src={footerLogo} className="md:w-2/12 w-3/12 -mr-10" />
+            <div className="flex justify-end gap-x-7">
+            {/* <Image alt="footerLogo" src={footerLogo} className="md:w-2/12 w-3/12 -mr-10" /> TODO: Amed Uzair */}
             <div className="md:w-[200px] w-[90px]">
               <TextAroundCircle
                 className="footer-circular-text"
                 id="footer-text-right-circle"
-                text=" WELCOME TO A NEW ERA OF CARE *** WELCOME TO A NEW ERA OF CARE ***"
+                text=" WELCOME TO A NEW ERA OF CARE *** WELCOME TO A NEW ERA OF CARE *** "
                 key={1} 
                 width={200}
                 height={200}
                 radius={43.3}
                 strokeColor="white"
                 strokeWidth={2}
-                fontSize={8}
+                fontSize={10}
               />
             </div>
           </div>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -17,6 +17,8 @@ import HorizontalSection from "@/Components/HorizontalSection"
 import HeroSection from "@/Components/HeroSection";
 import CardSection from "@/Components/CardSection";
 import Footer from "@/Components/Footer";
+import AnimatedButton from "@/Components/AnimatedButton";
+import AnimateWhenInViewport from "@/Components/AnimateWhenInViewport";
 
 const cardItems = [
   { id: 1, title: '', hashtags: ['#vegan', '#healthy'], socialAppIcon: twitterLogo.src, rectangle: rectangle.src, goVegan: goVegan.src, camera: camera.src, date: '15 Oct 2019', description: 'The Best Vegan Chocolate Cake- A quick and easy recipe! It’s super moist, rich and full of chocolate. ' },
@@ -30,48 +32,47 @@ const cardItems = [
 ];
 export default function Home() {
   const [hoveredColumn, setHoveredColumn] = useState<number | null>(null);;
-  // Define the radius for the text path and the inner circle
-  const textRadius = 120;
-  const innerCircleRadius = 102;  // Smaller radius for the inner circle
-
-  // Calculate the approximate circumference of the text path
-
-  // Prepare the text to cover the full circumference
-  const text = "Welcome to a New Era of Care * * * "; // Base text to display
-  const imageUrl = "/path/to/your/image.jpg";  // Path to your image file
-  const repeatedText = text.repeat(3); // Adjust repetition based on your text length and desired density
 
   return (
     <>
       <div>
-        {/* <HeroSection /> */}
+        <HeroSection />
       </div>
       <div>
-        {/* <CardSection /> */}
+        <CardSection />
       </div>
       <div style={{ backgroundColor: 'white'}}>
-        {/* <HorizontalSection /> */}
+        <HorizontalSection />
       </div>
-      <div className="">
-
+      <div>
 
         <section className="bg-[#D9D9D9]">
-          {/* <h1 className="" style={{ fontWeight: '860', fontFamily: "SFProDisplay, sans-serif" }}></h1>
-          
-          */}
-          {/* <AnimatedText sentences={sentences} /> */}
+          <AnimateWhenInViewport 
+              textToBeAnimated="ALL THAT WE DID!!!" 
+              cssClass="font-bold  md:text-[100px] text-[30px]" />
+
           <div className="flex justify-center">
-            {/* <AnimatedText text="ALL THAT WE DID!!!" customClassName="font-bold  md:text-[100px] text-[30px]  " /> */}
-          </div>
-          <div className="flex justify-center">
-            {/* <AnimatedText text="THAT YOU MUST HAVE SEEN" customClassName="text-center md:-mt-7 -mt-5 mb-10 md:text-[27.3px] text-[9px]" Style={{ fontWeight: '700', lineHeight: '32.52px', fontFamily: "SFProDisplay, sans-serif" }} /> */}
+            <AnimateWhenInViewport 
+              textToBeAnimated="ALL THAT WE DID!!!" 
+              cssClass="font-bold  md:text-[100px] text-[30px]" />
           </div>
 
-          {/* <TeamInformation /> */}
+          <div className="flex justify-center">
+
+            <AnimateWhenInViewport 
+              textToBeAnimated="THAT YOU MUST HAVE SEEN" 
+              cssClass="text-center md:-mt-7 -mt-5 mb-10 md:text-[27.3px] text-[9px]" 
+              styles={{ fontWeight: '700', lineHeight: '32.52px', fontFamily: "SFProDisplay, sans-serif" }} />
+
+          </div>
+
+          <TeamInformation />
+
           <div className="mt-20 flex justify-center">
-              {/* <SecondaryButton
-              outerClassName={"md:w-[40%] w-[100%] flex justify-center"}
-                customClassName={{
+            <div className="md:w-[40%] w-[100%] flex justify-center">
+            <AnimatedButton
+               text="SEE MORE"
+                customCssStyles={{
                   marginTop: "20px",
                   backgroundColor: "#DF1780",
                   border: "none",
@@ -86,41 +87,38 @@ export default function Home() {
                   width: "46%",
                   justifyContent: "space-between"
                 }}
-                text="SEE MORE"
-              /> */}
-          </div>
-
-        </section>
-
-
-
-
-        <section className="flex pb-10 bg-[#D9D9D9] flex-col md:flex-row">
-
-          <div className="md:w-4/12 flex md:flex-col ">
-            {/* <h3
-              className=""
-              style={}
-
-            ></h3> */}
-            <div className="w-7/12 md:w-full mt-14 md:mt-80">
-              {/* <AnimatedText text="Stay up-to-date on the
-              latest Ghmza and share
-              your thoughts." customClassName="md:w-4/12 w-6/12 md:ml-24 ml-6 md:text-[18.28px] text-[9.5px] md:leading-[32px] leading-[16.9px]" Style={{ fontWeight: '590', color: "#000000", fontFamily: "SFProDisplay, sans-serif", }} /> */}
-            </div>
-            <div className="w-6/12 md:mt-[75%] mt-12">
-              {/* <ExploreTheRest
-                topText="EXPLORE"
-                bottomText="THE REST"
-                topTextCustomClass="md:ml-32 md:text-[58px] text-[24.83px] flex justify-start"
-                bottomTextCustomClass="md:text-[58px] -mt-3 md:-mt-0 text-[24.83px] md:ml-[120px] md:flex md:font-[400]"
-                topTextStyle={{ fontWeight: '860', color: "#000000" }}
-                bottomTextStyle={{}} */}
+                arrowStyle={{ stroke: '#FFFFFF', marginLeft: '30px', display: 'flex', alignItems: 'center' }}
               />
             </div>
+          </div>
+        </section>
+
+        <section className="flex pb-10 bg-[#D9D9D9] flex-col md:flex-row">
+          <div className="md:w-4/12 flex md:flex-col ">
+            <div className="w-7/12 md:w-full mt-14 md:mt-80">
+
+              <AnimateWhenInViewport 
+              textToBeAnimated="Stay up-to-date on the
+              latest Ghmza and share
+              your thoughts." 
+              cssClass="md:w-4/12 w-6/12 md:ml-24 ml-6 md:text-[18.28px] text-[9.5px] md:leading-[32px] leading-[16.9px]" 
+              styles={{ fontWeight: '590', color: "#000000", fontFamily: "SFProDisplay, sans-serif" }} />
+
+            </div>
+            <div className="w-6/12 md:mt-[75%] mt-12">
+              <AnimateWhenInViewport 
+                textToBeAnimated='EXPLORE'
+                cssClass='md:ml-32 md:text-[58px] text-[24.83px] flex justify-start'
+                styles={{ fontWeight: '860', color: "#000000" }} />
+
+              <AnimateWhenInViewport 
+                textToBeAnimated='THE REST'
+                cssClass='md:text-[58px] -mt-3 md:-mt-0 text-[24.83px] md:ml-[120px] md:flex md:font-[400]'/>
+            </div>
             <div className="hidden md:block">
-              {/* <SecondaryButton
-                customClassName={{
+              <AnimatedButton
+               text="TAKE ME BACK"
+                customCssStyles={{
                   marginTop: "20px",
                   backgroundColor: "#DF1780",
                   border: "none",
@@ -136,13 +134,13 @@ export default function Home() {
                   marginLeft: "11%",
                   justifyContent: "space-between"
                 }}
-                text="TAKE ME BACK"
-              /> */}
+                arrowStyle={{ stroke: '#FFFFFF', marginLeft: '30px', display: 'flex', alignItems: 'center' }}
+              />
             </div>
           </div>
 
           <div className="hidden md:block">
-            {/* <motion.div layout className="grid grid-cols-3 relative mt-48 gap-x-[-8px !important] gap-y-[-4px]">
+            <motion.div layout className="grid grid-cols-3 relative mt-48 gap-x-[-8px !important] gap-y-[-4px]">
               {cardItems.map((card, index) => (
                 <Card
                   key={card.id}
@@ -152,14 +150,12 @@ export default function Home() {
                   onMouseLeave={() => setHoveredColumn(null)}
                 />
               ))}
-            </motion.div> */}
+            </motion.div>
           </div>
-    
         </section>
 
         <div className="bg-[#D9D9D9] block md:hidden ">
-          {/* <CustomSlider cards={cardItems} /> */}
-
+          {/* <CustomSlider cards={cardItems} />  TODO: Ahmed Uzair*/}
         </div>
         <Footer />
       </div>
