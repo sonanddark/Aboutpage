47922adbb6bfad2b4bee44232555a27dc06c5c5b
Uzair Ahmed <uzairahmed4705@gmail.com>
2024-07-07 18:58:26 +0500

    Add Scroll to section component

---
+++ src/Components/ScrollToSection.js
@@ -0,0 +1,9 @@
+const scrollToSection = (sectionId) => {
+    const section = document.getElementById(sectionId);
+    if (section) {
+      section.scrollIntoView({ behavior: 'smooth' });
+    }
+  };
+  
+  export default scrollToSection;
+  
