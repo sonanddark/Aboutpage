e7a522d85a6f230c75f9b26a44da119ce14cb3f8
Junaid Hassan <junaid.hassan@zeikh.com>
2024-07-08 00:18:51 +0500

    3rd section completed

---
+++ public/images/assets/index.ts
@@ -0,0 +1,4 @@
+export { default as SVG } from "./SVG.png";
+export { default as dog } from "./dog.png";
+export { default as manMountain } from "./manMountain.png";
+export { default as peoples } from "./peoples.png";
